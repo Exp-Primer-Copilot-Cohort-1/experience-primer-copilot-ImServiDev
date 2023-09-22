a82aaaec74dec3f7f65f3602ed5852c80a6f3598
ImServi <109850892+ImServiDev@users.noreply.github.com>
2023-09-22 10:41:55 +0000

    segundo

--- member.js
+++ member.js
@@ -1,6 +1,9 @@
 function skillsMember() {
-    return {
-        restrict: 'E',
-        templateUrl: 'templates/skills-member.html'
-    };
+    var member = document.getElementById("member");
+    var memberSkills = document.getElementById("memberSkills");
+    if (member.checked == true) {
+        memberSkills.style.display = "block";
+    } else {
+        memberSkills.style.display = "none";
+    }
 }
